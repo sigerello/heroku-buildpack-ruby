9dedb55955e270a5d35e36e73cc9619075e7637e
Oleksandr Serhiienko <sasha@bytesoft.io>
2018-07-05 05:35:13 +0300

    fix

--- lib/language_pack/installers/heroku_ruby_installer.rb
+++ lib/language_pack/installers/heroku_ruby_installer.rb
@@ -12,14 +12,22 @@ class LanguagePack::Installers::HerokuRubyInstaller
   end
 
   def fetch_unpack(ruby_version, install_dir, build = false)
+    if ruby_version.version == "ruby-1.9.3-p551" || ruby_version.version == "ruby-1.9.3"
+      base_url = "https://cache.ruby-lang.org/pub/ruby/1.9"
+      file = "ruby-1.9.3-p551.tar.gz"
+      fetcher = LanguagePack::Fetcher.new(base_url)
+    else
+      file = "#{ruby_version.version_for_download}.tgz"
+      fetcher = @fetcher
+    end
+
     FileUtils.mkdir_p(install_dir)
     Dir.chdir(install_dir) do
-      file = "#{ruby_version.version_for_download}.tgz"
       if build
         ruby_vm = "ruby"
         file.sub!(ruby_vm, "#{ruby_vm}-build")
       end
-      @fetcher.fetch_untar(file)
+      fetcher.fetch_untar(file)
     end
   end
 end
